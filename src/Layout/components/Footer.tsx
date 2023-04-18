ecd1fa1a61f42b89af25b32fd81c6ea51717cc78
Orxan Ulfatli <orxanulfatli@gmail.com>
2023-04-18 04:32:53 +0400

    fix footer

--- src/Layout/components/Footer.tsx
+++ src/Layout/components/Footer.tsx
@@ -2,15 +2,15 @@
 const Footer = () => {
   return (
     <div className="text-center bg-light py-3">
-      <h6>Welcome my chanel DevAT-VietNam</h6>
-      <a
+      <h6>Welcome my Blog</h6>
+      {/* <a
         href="https://www.youtube.com/c/DevATHTML"
         target="_blank"
         rel="noreferrer"
         className="mb-2 d-block"
       >
         https://www.youtube.com
-      </a>
+      </a> */}
       <p> Copyright &copy; 2023</p>
     </div>
   );
